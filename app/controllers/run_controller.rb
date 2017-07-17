60258186ea9ceafaa66cc21a8088a60c1781271d
CLackrone <charisaanne@gmail.com>
2017-07-17 09:53:15 -0400

    build login routes and form

--- app/controllers/run_controller.rb
+++ app/controllers/run_controller.rb
@@ -1,6 +1,7 @@
 class RunController < ApplicationController
 	get '/runs' do 
 		#Run.all is wrong, needs to pull just a single runner's runs
+		#@runs = current_user.runs ???
 		@runs = Run.all
 		erb :'/runs/runs'
 	end
--- app/controllers/runner_controller.rb
+++ app/controllers/runner_controller.rb
@@ -8,14 +8,20 @@ class RunnerController < ApplicationController
 		runner = Runner.new(:username => params[:username], :email => params[:email], :password => params[:password])
 		if runner.save
 			session[:user_id] = runner.id 
-			redirect 'runs'
+			redirect '/runs'
 		else
+			#this works - needs a flash message
 			redirect '/signup'
 		end
 	end
 
 	get '/login' do 
-		erb :'/runner/login'
+		if !logged_in?
+			erb :'/runner/login'
+		else
+			#add flash message
+			redirect '/runs'
+		end
 	end
 
 	post '/login' do 
